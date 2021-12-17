6ba7369093b57e02d753dc71551ef5edcd2b124d
domenique9 <51834820+domenique9@users.noreply.github.com>
2021-12-17 10:59:55 +0100

    higher min pragma to avoid compiling errors

--- brownie_unicash/contracts/StoreCharity.sol
+++ brownie_unicash/contracts/StoreCharity.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/math/SafeMath.sol";
